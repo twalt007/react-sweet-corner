35acfefa849ad5b4907a50f9d46ccc6c8f19962e
Tatumn Walter <tatumnwalter@gmail.com>
2019-09-13 06:37:10 -0700

    SweetCorner V1 Feature Set 1

---
+++ src/components/footer/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './footer.scss';
+
+export default props =>{
+    return (
+        <div>
+            <h1 className="center">This is the Footer</h1>
+        </div>
+    )
+}
---
+++ src/components/header/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './header.scss';
+
+export default props =>{
+    return (
+        <div>
+            <h1 className="center">This is the Header</h1>
+        </div>
+    )
+}
